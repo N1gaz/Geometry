1c77a28ed12d6d851524ad5552e8198b6a44005b
N1gaz <super.dartysh@gmail.com>
2024-01-21 21:38:14 +0300

    refactor
    rename

--- Geometry/Implementations/CommonGeometry.cs
+++ Geometry/Implementations/CommonGeometry.cs
@@ -2,7 +2,7 @@
 
 namespace Geometry.Implementations
 {
-    public static class Geometry
+    public static class CommonGeometry
     {
         public static double GetFigureArea(Figure figure) =>
             figure.Area;
