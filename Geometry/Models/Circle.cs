f5419288bdd6b369e44f548dc902b78c4198b0da
N1gaz <super.dartysh@gmail.com>
2024-01-21 20:40:01 +0300

    feature
    add ctor, change ctors' order

--- Geometry/Models/Circle.cs
+++ Geometry/Models/Circle.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Geometry.Abstracts;
 
 namespace Geometry.Models
 {
-    public class Circle
+    public class Circle : Figure
     {
+
+        public override double Area => throw new NotImplementedException();
     }
 }
--- Geometry/Models/Triangle.cs
+++ Geometry/Models/Triangle.cs
@@ -13,22 +13,28 @@ namespace Geometry.Models
         /// Создание треугольника по трем сторонам.
         /// </summary>
         /// <exception cref="ArgumentException"></exception>
-        public Triangle(double sideA, double sideB, double sideC)
-        {
-            _sides = (new List<double> { sideA, sideB, sideC }).OrderDescending().ToArray();
+        public Triangle(double sideA, double sideB, double sideC) : this(new[] {sideA,  sideB, sideC}) { }
 
-            if (!CheckEveryLength()) throw new ArgumentException("Некорректный ввод. Сторона может быть строго больше нуля.");
-            if (!CheckAllLengths()) throw new ArgumentException("Некорректный ввод. Одна сторона не может быть больше двух других");
+        public Triangle(IEnumerable<double> sides)
+        {
+            if (_sides is null || _sides.Count() != 3) throw new ArgumentException("У треугольника должно быть три стороны");
+
+            var sortedSides = sides.OrderDescending().ToArray();
+
+            if (!CheckEveryLength(sortedSides)) throw new ArgumentException("Некорректный ввод. Сторона может быть строго больше нуля.");
+            if (!CheckAllLengths(sortedSides)) throw new ArgumentException("Некорректный ввод. Одна сторона не может быть больше двух других");
+
+            _sides = sortedSides;
         }
 
-        private bool CheckEveryLength()
+        private bool CheckEveryLength(IEnumerable<double> sides)
         {
             foreach (var side in _sides)
                 if(!(side >  0)) return false;
             return true;
         }
 
-        private bool CheckAllLengths() => _sides[0] < _sides[1] + _sides[2];
+        private bool CheckAllLengths(IEnumerable<double> sortedSides) => sortedSides.ElementAt(0) < sortedSides.ElementAt(1) + sortedSides.ElementAt(2);
 
         private double Perimeter => _sides.Sum();
         public override double Area => Math.Sqrt(Perimeter * (Perimeter - _sides[0]) * (Perimeter - _sides[1]) * (Perimeter - _sides[2]));
