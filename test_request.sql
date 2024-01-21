e165b7b941e54caee44b25fab01f1aae5aae4cee
N1gaz <58673572+N1gaz@users.noreply.github.com>
2024-01-21 22:41:41 +0300

    Update test_request.sql
    add script for reading

--- test_request.sql
+++ test_request.sql
@@ -1,1 +1,5 @@
-
+SELECT p.ProductName, pcc.CategoryName FROM Product as p
+LEFT JOIN (SELECT pc.ProductId as pId, c.CategoryName FROM ProductCategory as pc
+INNER JOIN Category as c
+ON pc.CategoryId = c.Id) as pcc
+on pcc.pId = p.Id
