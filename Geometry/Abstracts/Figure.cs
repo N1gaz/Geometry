7d5675e4f627bb30cd7bc725de2b28fd064e65de
super <super@MYFUCKINGCUMMIN>
2024-01-21 19:29:32 +0300

    Добавьте файлы проекта.

---
+++ Geometry/Abstracts/Figure.cs
@@ -0,0 +1,13 @@
+﻿namespace Geometry.Abstracts
+{
+    /// <summary>
+    /// Базовый класс для всех фигур. По умолчанию любая фигура обязана реализовывать функцию площади.
+    /// </summary>
+    public abstract class Figure
+    {
+        /// <summary>
+        /// Площадь фигуры. Минимальная точность
+        /// </summary>
+        public abstract float Area { get; }
+    }
+}
---
+++ Geometry/Implementations/Geometry.cs
@@ -0,0 +1,15 @@
+﻿using Geometry.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry.Implementations
+{
+    public static class Geometry
+    {
+        public static float GetFigureArea(Figure figure) =>
+            figure.Area;
+    }
+}
