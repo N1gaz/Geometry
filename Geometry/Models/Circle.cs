01c7686bf100b12a3786309b6b71f0302a1f309f
N1gaz <super.dartysh@gmail.com>
2024-01-21 21:06:25 +0300

    features
    add circle's functionality

--- Geometry/Models/Circle.cs
+++ Geometry/Models/Circle.cs
@@ -4,7 +4,13 @@ namespace Geometry.Models
 {
     public class Circle : Figure
     {
+        private double _radius;
 
-        public override double Area => throw new NotImplementedException();
+        public Circle(double radius)
+        {
+            if (!(radius > 0)) throw new ArgumentException("Радиус строго положительный");
+            _radius = radius;
+        }
+        public override double Area => Math.PI * _radius * _radius;
     }
 }
